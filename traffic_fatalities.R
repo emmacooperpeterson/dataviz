c3af41fe2e7e60e5bbc9da5ca25ab582610f3810
Emma Peterson <emmacooperpeterson@gmail.com>
2017-09-26 16:46:52 -0500

    loading data

--- traffic_fatalities.R
+++ traffic_fatalities.R
@@ -4,3 +4,6 @@ library('dplyr')
 library('tidyr')
 library('stringr')
 library('ggplot2')
+
+acc2014 <- read_sas("~/Desktop/Repositories/dataviz/accident.sas7bdat")
+acc2015 <- read_csv("~/Desktop/Repositories/dataviz/accident.csv")
