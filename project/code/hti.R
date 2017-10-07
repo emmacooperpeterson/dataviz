70415bd849235a5abc40908235fdad4170351e2d
Emma Peterson <emmacooperpeterson@gmail.com>
2017-10-06 22:54:17 -0500

    one chart

--- project/code/hti.R
+++ project/code/hti.R
@@ -1,4 +1,4 @@
-library(haven)
+library(tidyverse)
 
 setwd('~/Desktop/Repositories/dataviz/project/data/')
 
@@ -38,7 +38,8 @@ ggplot(victim_services, aes(x=year, y=percentage, color=victimservices)) +
   
   labs(y = "Number of Countries Providing Victim Services", x = "Year", 
        title="Services for Victims of Human Trafficking",
-       subtitle="Majority of countries fail to provide services for victims") +
+       subtitle="Majority of countries fail to provide services for victims",
+       caption="Source: Human Trafficking Indicators, 2000-2011 (Harvard Dataverse)") +
   
   scale_color_manual(name="Victim\nServices",
                        values=c("#303841", "#F6C90E"), 
@@ -46,13 +47,16 @@ ggplot(victim_services, aes(x=year, y=percentage, color=victimservices)) +
 
   theme(panel.grid.minor.x=element_blank(),
         panel.grid.major.x=element_blank(),
+        plot.background = element_rect(fill="#F4F4F4"),
         legend.key = element_blank(),
         legend.title = element_text(family="Montserrat", size=10),
         legend.text = element_text(family="Courier New", size=8),
+        legend.background = element_rect(fill="#F4F4F4"),
         plot.title = element_text(family="Montserrat", face="bold", size=15),
         plot.subtitle = element_text(family="Courier New"),
-        panel.background = element_rect(fill = "white"),
-        panel.grid.major = element_line(color="gray", size=0.25),
+        panel.background = element_rect(fill = "#F4F4F4"),
+        panel.grid.major.y = element_line(color="black", size=0.25),
+        panel.grid.minor.y = element_blank(),
         axis.ticks.y = element_line(size=0),
         axis.ticks.x = element_line(size=0),
         axis.text.x = element_text(angle = 45, hjust = 1, margin=margin(t=-2)),
@@ -60,13 +64,14 @@ ggplot(victim_services, aes(x=year, y=percentage, color=victimservices)) +
         axis.title = element_text(family="Montserrat", size=8),
         axis.title.x = element_text(margin=margin(t=15)),
         axis.title.y = element_text(margin=margin(r=15)),
-        plot.margin=unit(c(1,1,1,1),"cm")) +
+        plot.margin=unit(c(1,1,1,1),"cm"),
+        plot.caption = element_text(family="Montserrat Light", size=6, 
+                                    margin=margin(t=20))) +
 
   scale_x_continuous(breaks=seq(2001,2011),
                      labels=seq(2001,2011)) +
 
   scale_y_continuous(breaks=c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6),
                      labels=c("10%", "20%", "30%", "40%", "50%", "60%"))
-
 
 
